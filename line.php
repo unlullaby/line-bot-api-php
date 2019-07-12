34dffe297a4a35f989b22207f36b1f692f2f81fc
unlullaby <52811368+unlullaby@users.noreply.github.com>
2019-07-12 14:57:05 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'NYSdh5YhoHq+FeW3sHsUiq2fQVFCvSBcre3PEGrnw7lJcOnkl557zPmxC3dls5SeHPABXSZSP25fzGBiTjL80uTowLp2MuXnQrj1sWi2sOveZMth20FCUDjWAVy6QyQAjehM2AWkAN+3fjnCZMOSvAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
